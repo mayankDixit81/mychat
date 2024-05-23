8bd4f2d98d3acb26283b5276d931d7f6cce00081
mayankDixit81 <mayankkcnit@gmail.com>
2024-05-23 11:57:14 +0530

    streamlit version added

--- mychatbot.py
+++ mychatbot.py
@@ -11,7 +11,8 @@ import os
 # Load environment variables from '.env' file
 load_dotenv()
 # Accessing variables
-OPENAI_API_KEY = os.getenv("API_KEY")
+#OPENAI_API_KEY = os.getenv("API_KEY")
+OPENAI_API_KEY = "sk-proj-yuToFiDpzhxlcqo696i8T3BlbkFJMsJi087XtCbvC1SxVMvN"
 st.write("Deployed Streamlit version:", st.__version__)
 # Function to update message log
 def update_message_log(user_input):
